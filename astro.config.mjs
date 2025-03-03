758d0f04c3d9111be538a9c48cf3cdd98314b516
efrainsiccha <efrainsicchasql@gmail.com>
2025-03-02 20:47:25 -0500

    Actualizacion para el despliegue de mi portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://efrainsiccha.github.io',
+	base: 'portfolio-dev',
 	integrations: [tailwind()],
 });
